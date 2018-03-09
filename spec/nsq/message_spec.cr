639c1675b811909066731af761f27b2bb8ae29f8
Filipe Correia <filipe.lp.correia@gmail.com>
2018-03-09 21:21:21 +0000

    Adds ameba

--- spec/nsq/message_spec.cr
+++ spec/nsq/message_spec.cr
@@ -3,7 +3,7 @@ require "json"
 
 macro tcp_server_eq(message, &block)
   channel = Channel(Nil).new
-  server = TCPServer.new("localhost", 51234)
+  server = TCPServer.new("localhost", 51_234)
   spawn do
     server.accept do |client|
       message = client.gets
@@ -20,17 +20,17 @@ end
 module NSQ
   describe Message do
     it "initializes" do
-      message = Message.new(attempts: 1.to_u16, id: "1", timestamp: 10000.to_i64, body: "body", connection: nil)
+      message = Message.new(attempts: 1.to_u16, id: "1", timestamp: 10_000.to_i64, body: "body", connection: nil)
       message.attempts.should eq 1
       message.id.should be "1"
-      message.timestamp.should eq 10000
+      message.timestamp.should eq 10_000
       message.body.should be "body"
     end
 
     it "touches" do
       tcp_server_eq("TOUCH 1234") do |host|
         conn = Connection.new host
-        message = Message.new(attempts: 1.to_u16, id: "1234", timestamp: 10000.to_i64, body: "body", connection: conn)
+        message = Message.new(attempts: 1.to_u16, id: "1234", timestamp: 10_000.to_i64, body: "body", connection: conn)
         message.touch
       end
     end
@@ -38,7 +38,7 @@ module NSQ
     it "requeues" do
       tcp_server_eq("REQ 1234 0") do |host|
         conn = Connection.new host
-        message = Message.new(attempts: 1.to_u16, id: "1234", timestamp: 10000.to_i64, body: "body", connection: conn)
+        message = Message.new(attempts: 1.to_u16, id: "1234", timestamp: 10_000.to_i64, body: "body", connection: conn)
         message.requeue
       end
     end
