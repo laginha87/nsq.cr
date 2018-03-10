2496ff62b5afe078c1c9d26f5eeec1b82d31b1d4
Filipe Correia <filipe.lp.correia@gmail.com>
2018-03-10 17:23:08 +0000

    Adds a simple example

---
+++ examples/channel_subscribe.cr
@@ -0,0 +1,26 @@
+# Subscribes to the channel and it ouptuts everything
+require "../src/nsq"
+
+puts "Getting started"
+
+client = NSQ::Client.new("localhost:9002")
+
+callback = ->(message : NSQ::Message) do
+    puts message.body
+    message.finish
+end
+
+
+client.subscribe("topic_1", "channel_1", callback)
+
+def send_message(topic, message)
+    `curl -d '#{message}' 'localhost:9001/pub?topic=#{topic}' &> /dev/null`
+end
+
+1.upto(100).each do |e|
+    send_message("topic_1", "Hello #{e}")
+end
+
+loop do
+    sleep(10)
+end
