df1d09cfad39a159fbb5e1a768b1cc67e21ccedb
Filipe Correia <filipe.lp.correia@gmail.com>
2018-03-10 17:25:45 +0000

    Adds ci fix

--- examples/channel_subscribe.cr
+++ examples/channel_subscribe.cr
@@ -6,21 +6,20 @@ puts "Getting started"
 client = NSQ::Client.new("localhost:9002")
 
 callback = ->(message : NSQ::Message) do
-    puts message.body
-    message.finish
+  puts message.body
+  message.finish
 end
-
 
 client.subscribe("topic_1", "channel_1", callback)
 
 def send_message(topic, message)
-    `curl -d '#{message}' 'localhost:9001/pub?topic=#{topic}' &> /dev/null`
+  `curl -d '#{message}' 'localhost:9001/pub?topic=#{topic}' &> /dev/null`
 end
 
 1.upto(100).each do |e|
-    send_message("topic_1", "Hello #{e}")
+  send_message("topic_1", "Hello #{e}")
 end
 
 loop do
-    sleep(10)
+  sleep(10)
 end
